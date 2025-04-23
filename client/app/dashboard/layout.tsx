fa5f02a1bcdd783e47be01da9284de216070f277
Dharmapra2 <dharmapra865@gmail.com>
2025-04-24 01:30:54 +0530

    working on UI section

---
+++ client/app/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function DashBoardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main
+      className={`h-[90%] w-[90%] text-white rounded-xl flex flex-row justify-between items-center gap-6`}
+    >
+      {/* left section */}
+      <section className="bg-primary grow w-[75%] h-full rounded-md">{children}</section>
+
+      {/* Right section */}
+      <nav className="bg-primary grow-0 w-[25%] h-full rounded-md">
+        name asds
+        <button type="button">create game</button>
+        <button type="button">create</button>
+        <p>in progress</p>
+      </nav>
+    </main>
+  );
+}
---
+++ client/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>Dashbord page</div>;
+}
+
+export default page;
